ff6b61279b67c9a58b06cf2292c5baa3b47cce98
Kate Shirley <KShirley@C-K.com>
2018-02-24 17:02:00 -0600

    button toggles and other stuff

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -12,14 +12,22 @@
 
   $(function () {
     var $homeClick=$('#home-scroll');
-    var $page=$('html, body')
-    console.log('hi');
+    var $page=$('html, body');
+
+    var $formButton=$('.btn-services');
+
 
     $homeClick.click(function(){
       $page.animate({
         scrollTop: $homeClick.offset().top
     }, 800);
   });
-    });
+
+  $formButton.click(function(e){
+    console.log('form button click');
+    e.preventDefault();
+    $(this).toggleClass('btn-services-active');
+  })
+});
 
 })(jQuery, window, document);
